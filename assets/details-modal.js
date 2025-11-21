2e4b9ca51085c2fd0810682fa9fef1870903c6c6
Matthew Durey <matthew@thegcgroup.co.uk>
2025-11-21 16:27:30 +0000

    hover fix on mobile

--- assets/details-modal.js
+++ assets/details-modal.js
@@ -7,6 +7,7 @@ class DetailsModal extends HTMLElement {
     this.detailsContainer.addEventListener('keyup', (event) => event.code.toUpperCase() === 'ESCAPE' && this.close());
     this.summaryToggle.addEventListener('click', this.onSummaryClick.bind(this));
     this.querySelector('button[type="button"]').addEventListener('click', this.close.bind(this));
+    console.log('fgvhbjnmk');
 
     this.summaryToggle.setAttribute('role', 'button');
   }
